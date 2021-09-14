bcf1588b47cdeaea456fb56d1e3015de9c711ac6
Seiji Matsumoto <seijim27@gmail.com>
2021-09-13 23:58:22 -0400

    Changed brightness to 30%

--- comp/Navbar.tsx
+++ comp/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar: NextPage = () => {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous" />
       </Head>
       <nav className={styles.nav}>
         <div className={styles.mainLinks}>
