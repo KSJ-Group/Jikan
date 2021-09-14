da40f2d8f494efc7adec14f5459613b4d1a3c644
katscap <katielaw97@gmail.com>
2021-09-14 18:09:56 -0400

    hooked up search bar to pexel api

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styles from '../styles/Search.module.css';
+
+interface Photo {
+  url: string,
+  avg_color: string,
+}
+
+const photos: Photo[] = [];
 
 const Search = () => {
   const [terms, setTerms] = useState<string>('')
+  const [page, setPage] = useState<number>(1)  // this should increment when user wants to see next set of images
+  const [images, setImages] = useState<typeof photos>([])
 
   useEffect(() => {
-    requestPexel()
+    fetchImages(terms, page)
   },[terms])
 
   const search = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -13,11 +23,15 @@ const Search = () => {
     setTerms(event.target.value)
   }
 
-  const requestPexel = () => {
-
+  const fetchImages = (searchTerms: string, pageNumber: number) => {
+    axios.get(`/api/images?terms=${searchTerms}&page=${page}`)
+      .then((data) => {
+        setImages(data.data)
+      })
+      .catch((error) => {console.log(error)})
   }
 
-
+console.log(images)
   return (
     <div className={styles.search}>
       <form>
--- pages/api/images/index.ts
+++ pages/api/images/index.ts
@@ -1,17 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios, { AxiosRequestConfig } from 'axios';
+import { pexelAPI } from '../../../config.js';
 
-type Data = {
-  image: string
+
+interface Photo {
+  url: string,
+  avg_color: string,
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  if (req.method === 'REQUEST')
-    return new Promise((resolve, reject) => {
-      resolve(res.status(200).json({ image: 'this should be a string'}))
-    }).catch((error) => {console.log(error)})
+const photos: Photo[] = [];
 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<typeof photos>
+) {
+  // request pexels api. terms are located on req.query.terms
+  let { terms, page } = req.query
+  let query: AxiosRequestConfig = {
+    method: `get`,
+    url: `https://api.pexels.com/v1/search?query=${terms}&page=${page}&per_page=12`,
+    headers: {Authorization: pexelAPI}
+  }
+
+  // 'headers': {'Authorization': `${API_KEY}`}
+
+  axios(query)
+    .then((response) => {console.log(response.data); res.status(200).json(response.data.photos)})
+    .catch((error) => {console.log(error)})
 
 }
