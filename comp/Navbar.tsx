f213f8001aaf3fb065142f3109c96428bbf6649c
Seiji Matsumoto <seijim27@gmail.com>
2021-09-13 23:19:46 -0400

    Added full screen button

--- comp/Navbar.tsx
+++ comp/Navbar.tsx
@@ -3,26 +3,59 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image'
 import styles from '../styles/Navbar.module.css'
+import Head from 'next/head';
 
 const Navbar: NextPage = () => {
-  const clickClock = () => {
-    document.querySelector('.link1')?.classList.add('activeLink');
-    document.querySelector('.link2')?.classList.remove('activeLink');
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const [clockIsActive, setClockIsActive] = useState(true);
+  const [pomodoroIsActive, setPomodoroIsActive] = useState(false);
+
+  const openFullscreen = () => {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    }
+    setIsFullscreen(true);
   }
 
-  const clickTimer = () => {
-    document.querySelector('.link1')?.classList.remove('activeLink');
-    document.querySelector('.link2')?.classList.add('activeLink');
+  const closeFullscreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+    setIsFullscreen(false);
   }
+
+  const clickLink = () => {
+    setClockIsActive(!clockIsActive);
+    setPomodoroIsActive(!pomodoroIsActive);
+  }
+
+  useEffect(() => {
+    if (clockIsActive) {
+      document.getElementById('link1')!.classList.add('activeLink');
+      document.getElementById('link2')!.classList.remove('activeLink');
+    } else {
+      document.getElementById('link2')!.classList.add('activeLink');
+      document.getElementById('link1')!.classList.remove('activeLink');
+    }
+  }, [clockIsActive])
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.mainLinks}>
-        <Link href="/"><a className={styles.link} id='link1' onClick={clickClock}>Digital Clock</a></Link>
-        <div className={styles.line}>|</div>
-        <Link href="/pomodoro"><a className={styles.link} id='link2' onClick={clickTimer}>Pomodoro Timer</a></Link>
-      </div>
-    </nav>
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"/>
+      </Head>
+      <nav className={styles.nav}>
+        <div className={styles.mainLinks}>
+          <Link href="/"><a className={styles.link} id='link1' onClick={clickLink}>Digital Clock</a></Link>
+          <div className={styles.line}>|</div>
+          <Link href="/pomodoro"><a className={styles.link} id='link2' onClick={clickLink}>Pomodoro Timer</a></Link>
+          {isFullscreen ? <div onClick={() => closeFullscreen()} className={styles.line}>[  ]</div> :
+            <div onClick={() => openFullscreen()} className={styles.line}>[  ]</div>}
+        </div>
+
+      </nav>
+    </>
   );
 };
 
