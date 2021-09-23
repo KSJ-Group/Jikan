f8f53049eda6ea59f71f77017a6c91f246165b8f
Seiji Matsumoto <seijim27@gmail.com>
2021-09-22 20:02:30 -0400

    Added ability to add background blur

--- styles/global.style.ts
+++ styles/global.style.ts
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
 
 interface Props {
-  brightness: any;
+  brightness: any
+}
+
+interface Background {
+  blur: any
 }
 
 export const BrightnessDiv = styled.div<Props>`
   height: 100vh;
   width: 100vw;
-  filter: brightness(${props => props.brightness})
+  filter: brightness(${props => props.brightness});
+`;
+
+export const BackgroundBlur = styled.div<Background>`
+  filter: blur(${props => props.blur || '0'});
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  transition: 0.6s ease-in-out;
 `;
