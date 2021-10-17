e23fd4affa24fc6a4e7bf4443dbf5d0fd5fc8264
Seiji Matsumoto <seijim27@gmail.com>
2021-10-17 13:59:35 -0400

    Added initial graphql

--- pages/api/images/index.ts
+++ pages/api/images/index.ts
@@ -1,11 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import axios, { AxiosRequestConfig } from 'axios';
-require('dotenv').config();
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios, { AxiosRequestConfig } from "axios";
+require("dotenv").config();
 
 interface Photo {
-  url: string,
-  avg_color: string,
-};
+  url: string;
+  avg_color: string;
+}
 
 const photos: Photo[] = [];
 
@@ -13,13 +13,19 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<typeof photos>
 ) {
-  let { terms, page } = req.query
+  let { terms, page } = req.query;
   let query: AxiosRequestConfig = {
     method: `get`,
     url: `https://api.pexels.com/v1/search?query=${terms}&page=${page}&per_page=12&orientation=landscape`,
-    headers: {Authorization: process.env.pexelAPI}
-  }
+    headers: { Authorization: process.env.pexelAPI },
+  };
   return axios(query)
-    .then((response) => {res.status(200).json(response.data);})
-    .catch((error) => {console.log(error);})
-};
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// export const auth: string = process.env.local.AUTH!;
