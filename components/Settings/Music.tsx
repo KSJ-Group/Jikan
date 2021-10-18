6477eb04c798dafc79d316404bd596bcc0ff73b5
Seiji Matsumoto <seijim27@gmail.com>
2021-10-18 12:36:50 -0400

    Added more music

--- components/Settings/Music.tsx
+++ components/Settings/Music.tsx
@@ -14,16 +14,24 @@ const Music: React.FC<Props> = ({ selectedMusic, setMusic }) => {
       url: 'None'
     },
     {
-      title: 'Lofi Hip Hop',
-      url: '//www.youtube.com/embed/5qap5aO4i9A?autoplay=1'
-    },
-    {
-      title: 'Weekend Jazz',
-      url: '//www.youtube.com/embed/uKTUW9niMYg?autoplay=1'
+      title: 'Anime Lofi',
+      url: '//www.youtube.com/embed/w3LWHIz3bMc?autoplay=1'
     },
     {
       title: 'Calm Piano',
       url: '//www.youtube.com/embed/XULUBg_ZcAU?autoplay=1'
+    },
+    {
+      title: 'Deep Focus',
+      url: '//www.youtube.com/embed/8N-eLvmheSE?autoplay=1'
+    },
+    {
+      title: 'Lofi Hip Hop',
+      url: '//www.youtube.com/embed/5qap5aO4i9A?autoplay=1'
+    },
+    {
+      title: 'Nintendo Radio',
+      url: '//www.youtube.com/embed/tOnOutGHcRQ?autoplay=1'
     },
     {
       title: 'Relaxing Sleep',
@@ -38,13 +46,17 @@ const Music: React.FC<Props> = ({ selectedMusic, setMusic }) => {
       url: '//www.youtube.com/embed/gfo2xZ2SMjc?autoplay=1'
     },
     {
-      title: 'Deep Focus',
-      url: '//www.youtube.com/embed/8N-eLvmheSE?autoplay=1'
-    },
-    {
       title: 'Spooky Halloween Music',
       url: '//www.youtube.com/embed/qJSLmjzLnAM?autoplay=1'
-    }
+    },
+    {
+      title: 'The Good Life',
+      url: '//www.youtube.com/embed/36YnV9STBqc?autoplay=1'
+    },
+    {
+      title: 'Weekend Jazz',
+      url: '//www.youtube.com/embed/uKTUW9niMYg?autoplay=1'
+    },
   ]
   const [availMusic, setAvailMusic] = useState<{ title: string, url: string }[]>(music);
   const [currentTitle, setCurrent] = useState<string>('');
