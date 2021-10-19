b665da382a65236563a4b77bad72aa948c40edeb
Seiji Matsumoto <seijim27@gmail.com>
2021-10-19 08:56:37 -0400

    Added pop-up youtube player for mobile

--- components/YouTube.jsx
+++ components/YouTube.jsx
@@ -2,14 +2,20 @@ import React, { useState, useContext, useEffect } from "react";
 import styles from "../styles/Main/Main.module.css";
 import { SettingsContext } from "./SettingsContext";
 import YouTube from "react-youtube";
+import { Offcanvas } from "react-bootstrap";
 
 const YouTubePlayer = () => {
   const { selectedMusic, musicVolume } = useContext(SettingsContext);
   const [player, setPlayer] = useState(null);
+  const [showYoutube, setShowYoutube] = useState(false);
 
   const config = {
-    height: "0",
-    width: "0",
+    height: "200px",
+    width: "350px",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
   };
 
   const _onReady = (event) => {
@@ -36,12 +42,18 @@ const YouTubePlayer = () => {
   }, [selectedMusic]);
 
   return (
-    <YouTube
-      className={styles.iframe}
-      videoId={selectedMusic}
-      opts={config}
-      onReady={_onReady}
-    />
+    <div className={styles.youtube}>
+      <div className={styles.sideBtn} onClick={() => setShowYoutube(true)}>
+        <YouTube
+          id="youtube-player"
+          className={styles.iframe}
+          videoId={selectedMusic}
+          opts={config}
+          onReady={_onReady}
+        />
+        ♫
+      </div>
+    </div>
   );
 };
 
