5376b27632ea355e93bf6b264de013dc64722157
Seiji Matsumoto <seijim27@gmail.com>
2021-10-20 10:12:46 -0400

    Removed login from settings

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -59,7 +59,6 @@ const Settings: React.FC<Props> = ({ showSettings, setShowSettings }) => {
   } = useContext(SettingsContext);
 
   const [isMobile, setIsMobile] = useState<boolean>(false);
-  // const [sliderClicked, setClicked] = useState<boolean>(false);
 
   useEffect(() => {
     if (
@@ -73,52 +72,6 @@ const Settings: React.FC<Props> = ({ showSettings, setShowSettings }) => {
     }
   }, []);
 
-  // useEffect(() => {
-  //   if (showSettings) {
-  //     swipe();
-  //   }
-  // }, [showSettings])
-
-  // const swipe = () => {
-  //   document.addEventListener('touchstart', handleTouchStart, false);
-  //   document.addEventListener('touchmove', handleTouchMove, false);
-  //   var xDown = null;
-  //   var yDown = null;
-
-  //   function getTouches(evt) {
-  //     return evt.touches ||             // browser API
-  //       evt.originalEvent.touches; // jQuery
-  //   }
-
-  //   function handleTouchStart(evt) {
-  //     const firstTouch = getTouches(evt)[0];
-  //     xDown = firstTouch.clientX;
-  //     yDown = firstTouch.clientY;
-  //   };
-
-  //   function handleTouchMove(evt) {
-  //     if (!xDown || !yDown) {
-  //       return;
-  //     }
-
-  //     var xUp = evt.touches[0].clientX;
-  //     var yUp = evt.touches[0].clientY;
-
-  //     var xDiff = xDown - xUp;
-  //     var yDiff = yDown - yUp;
-
-  //     if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
-  //       if (xDiff < 0 && !sliderClicked) {
-  //         console.log('right swipe!')
-  //         setShowSettings(false);
-  //       }
-  //     }
-  //     /* reset values */
-  //     xDown = null;
-  //     yDown = null;
-  //   };
-  // }
-
   return (
     <div className={styles.settingsDiv} id="touchsurface">
       <Offcanvas
@@ -129,7 +82,7 @@ const Settings: React.FC<Props> = ({ showSettings, setShowSettings }) => {
       >
         <Offcanvas.Body className={styles.body}>
           <div className={styles.subBody}>
-            <Login />
+            {/* <Login /> */}
             {isClock ? (
               // Clock settings
               <div>
