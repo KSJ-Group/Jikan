857d68e9e286eb548e31a7cbefccde095def35b8
Seiji Matsumoto <seijim27@gmail.com>
2021-10-20 13:11:15 -0400

    Changed default background

--- components/BackgroundContext.tsx
+++ components/BackgroundContext.tsx
@@ -2,7 +2,7 @@ import React, { useState, createContext, useContext, useEffect } from 'react';
 
 export const BackgroundContext = createContext(
   {
-    background: '/images/mountains.jpeg',
+    background: '/images/fall.jpg',
     changeBackground: (url: string): void => { },
     loaded: true,
     changeLoadStatus: (status: boolean): void => { },
@@ -11,7 +11,7 @@ export const BackgroundContext = createContext(
 
 export const BackgroundProvider: React.FC = ({ children }) => {
   const [backgroundType, setType] = useState<string>('image');
-  const [background, setBackground] = useState<string>('/images/mountains.jpeg');
+  const [background, setBackground] = useState<string>('/images/fall.jpg');
   const [loaded, setLoaded] = useState(true);
 
   const store = {
