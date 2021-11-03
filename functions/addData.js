9fc3d8342ca2ce145a2bdc6c6a5e2febe38c9d56
Seiji Matsumoto <seijim27@gmail.com>
2021-11-03 19:13:31 -0400

    Added initial configurations for serverless database

---
+++ functions/addData.js
@@ -0,0 +1,35 @@
+const { createClient } = require("@astrajs/collections");
+
+exports.handler = async function (event, context) {
+  const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    applicationToken: process.env.ASTRA_DB_TOKEN,
+    username: process.env.ASTRA_DB_USERNAME,
+    password: process.env.ASTRA_DB_PASSWORD,
+  });
+
+  const usersCollection = astraClient
+    .namespace(process.env.ASTRA_DB_KEYSPACE)
+    .collection("users");
+
+  console.log(usersCollection);
+
+  // create a new user (specifying documentId)
+  try {
+    const user = await usersCollection.create("cliff@wicklow.com", {
+      name: "cliff",
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(user),
+    };
+    console.log("Success");
+  } catch (e) {
+    console.log("Error", e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };
+  }
+};
