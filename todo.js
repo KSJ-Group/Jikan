8dc83292d99bfd75780d929a170db4d385328114
Seiji Matsumoto <seijim27@gmail.com>
2021-11-04 00:53:35 -0400

    Added TODO

---
+++ todo.js
@@ -0,0 +1,17 @@
+/**
+ * --- TODO LIST ----
+ * [] Add Spotify API
+ * [] Create a user stats page with number of pomodoro cycles complete, calendar, graphs, and charts
+ * [] Create a React Native App
+ * [] Create a Chrome Extension
+ * [X] Create database and connect to frontend
+ * [X] Add authentication
+ * [X] Add Weather API
+ * [X] Add pop-up youtube player off screen
+ * [X] Create an About Page
+ * [X] Add music - YouTube API?
+ * [X] Add modal that allows user to start short or long break after timer ends
+ * [X] Add toggles for auto-start short/long break
+ * [X] Add sound preview button
+ * [X] Allow alarm tones to be customized
+ */
