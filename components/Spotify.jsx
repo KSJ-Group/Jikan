4fda7ddcb588111cb75f0f09b96daec1d29889e0
Seiji Matsumoto <seijim27@gmail.com>
2022-02-28 14:36:07 -0500

    Add console.log

--- components/Spotify.jsx
+++ components/Spotify.jsx
@@ -31,6 +31,7 @@ const Spotify = () => {
       return accumulater;
     }, {});
 
+    console.log(paramsSplitUp);
     return paramsSplitUp;
   };
 
@@ -38,8 +39,6 @@ const Spotify = () => {
     if (window.location.hash) {
       const { access_token, expires_in, token_type } =
         getReturnedParamsFromSpotifyAuth(window.location.hash);
-      console.log(window.location.hash);
-      console.log("Access token:", access_token);
       setToken(access_token);
       localStorage.setItem("accessToken", access_token);
       localStorage.setItem("tokenType", token_type);
