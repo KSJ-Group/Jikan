28e5192c114198d8841f52701f7b5f367e52a391
Seiji Matsumoto <seijim27@gmail.com>
2022-02-28 14:45:07 -0500

    Change redirect uri

--- components/Spotify.jsx
+++ components/Spotify.jsx
@@ -40,9 +40,9 @@ const Spotify = () => {
       const { access_token, expires_in, token_type } =
         getReturnedParamsFromSpotifyAuth(window.location.hash);
       setToken(access_token);
-      localStorage.setItem("accessToken", access_token);
-      localStorage.setItem("tokenType", token_type);
-      localStorage.setItem("expiresIn", expires_in);
+      // localStorage.setItem("accessToken", access_token);
+      // localStorage.setItem("tokenType", token_type);
+      // localStorage.setItem("expiresIn", expires_in);
     } else {
       // if (localStorage.getItem("accessToken")) {
       //   handleLogin();
