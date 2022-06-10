1e51079c3de40c0ba4b70a416a54a1a1ae209a0e
SeijiMatsumoto <seijim27@gmail.com>
2022-06-10 17:44:54 -0400

    Add reset localstorage logic

--- components/Settings/Reset.tsx
+++ components/Settings/Reset.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import globalStyles from '../../styles/Settings/Settings.module.css'
+import styled from 'styled-components';
 
-const Reset = () => {
-  const reset = (e: any) => {
-    e.preventDefault();
-    localStorage.clear();
-    location.reload();
+const Button = styled.button`
+  font-size: 12px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #efefef;
+  border: 0.1px solid rgb(168, 168, 168);
+  transition: 0.3s ease-in-out;
+  &:hover {
+    background: #dddddd;
   }
+`
+const Reset = () => {
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  const areYouSure = (e: any) => {
+    e.preventDefault();
+    setShowConfirm(true);
+  }
+
+  const clickReset = (e: any) => {
+    setTimeout(() => {
+      localStorage.clear();
+      location.reload();
+    }, 1000);
+  }
+
   return (
-    <button className={globalStyles.settingModuleContainer} onClick={(e: any) => reset(e)}>
-      Reset All Options
-    </button>
+    <div className={globalStyles.settingModuleContainer}>
+      {showConfirm ?
+        <Button onClick={(e: any) => clickReset(e)}>Are you sure?</Button> :
+        <Button onClick={(e: any) => areYouSure(e)}>
+          Reset All Options
+        </Button>
+      }
+
+    </div>
   );
 };
 
