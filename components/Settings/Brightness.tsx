095b48929dd05e01ae8cb7c8fc3b3600a9ecec90
SeijiMatsumoto <seijim27@gmail.com>
2022-06-12 15:04:50 -0400

    Combine brightness and opacity

--- components/Settings/Brightness.tsx
+++ components/Settings/Brightness.tsx
@@ -25,19 +25,19 @@ const Brightness: React.FC<Props> = ({ brightness, setBrightness, opacity, setOp
     <div className={globalStyles.settingModuleContainer}>
       <div className={styles.container}>
         <div id='brightness' className={styles.brightnessContainer}>
-          Brightness
-          <div id='sliderdiv' className={globalStyles.sliderDiv}>
-            <input id="slider" className={globalStyles.slider} defaultValue={brightness} onChange={changeHandler} type="range" name="brightness"
-              min="5" max="100" />
-            <div>{brightness}%</div>
-          </div>
-        </div>
-        <div id='brightness' className={styles.brightnessContainer}>
           Opacity
           <div id='sliderdiv' className={globalStyles.sliderDiv}>
             <input id="slider" className={globalStyles.slider} defaultValue={opacity} onChange={changeHandler2} type="range" name="opacity"
               min="10" max="60" />
             <div>{opacity}%</div>
+          </div>
+        </div>
+        <div id='brightness' className={styles.brightnessContainer}>
+          Brightness
+          <div id='sliderdiv' className={globalStyles.sliderDiv}>
+            <input id="slider" className={globalStyles.slider} defaultValue={brightness} onChange={changeHandler} type="range" name="brightness"
+              min="5" max="100" />
+            <div>{brightness}%</div>
           </div>
         </div>
       </div>
