51bf49c67a98c8a92569e418fb2b5ec4f3e4c570
Seiji Matsumoto <seijim27@gmail.com>
2023-08-05 18:35:34 -0400

    Rename import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Clock from "../src/Components/clock";
+import Clock from "../src/Components/Clock";
 import styles from "../src/styles/Main/Main.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
