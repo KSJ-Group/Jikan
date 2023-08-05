2dc66d158db1a18ffb7b15e41272051bfc688b31
Seiji Matsumoto <seijim27@gmail.com>
2023-08-05 18:42:39 -0400

    Add get and post to firebase

--- src/contexts/SettingsContext.tsx
+++ src/contexts/SettingsContext.tsx
@@ -1,6 +1,10 @@
 import React, { useState, createContext, useContext, useEffect } from "react";
-import axios from "axios";
 import firebase from '../firebase';
+import { getSettingsFromFirebase, getSettingsFromLocalStorage } from "../helper/getSettings";
+import checkMobile from "../helper/checkMobile";
+import getAlarms from "../helper/getAlarms";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 interface Weather {
   city: string
@@ -11,12 +15,14 @@ interface Weather {
 }
 
 export const SettingsContext = createContext({
+  // default values
+  isLoading: true,
   isClock: true,
   setIsClock: (isClock: boolean) => { },
   pomodoroTime: 1500000,
   setPomodoroTime: (time: any) => { },
-  shortBreakTime: 300000,
-  setShortBreakTime: (time: any) => { },
+  breakTime: 300000,
+  setBreakTime: (time: any) => { },
   autoStartBreak: true,
   setAutoStartBreak: (auto: boolean) => { },
   showSeconds: false,
@@ -62,13 +68,28 @@ export const SettingsContext = createContext({
 });
 
 export const SettingsProvider: React.FC = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [isClock, setIsClock] = useState<boolean>(true);
   const [pomodoroTime, setPomodoroTime] = useState<number>(1500000);
-  const [shortBreakTime, setShortBreakTime] = useState<number>(300000);
+  const [breakTime, setBreakTime] = useState<number>(300000);
   const [autoStartBreak, setAutoStartBreak] = useState<boolean>(true);
   const [showSeconds, setShowSeconds] = useState<boolean>(false);
   const [is24Hour, setIs24Hour] = useState<boolean>(false);
   const [selectedAlert, setSelectedAlert] = useState<string>("Xylophone.mp3");
+  const [musicVolume, setMusicVolume] = useState<number>(100);
+  const [alertVolume, setAlertVolume] = useState<number>(100);
+  const [zip, setZip] = useState<string>('');
+  const [showSettings, setShowSettings] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [started, setStarted] = useState<boolean>(false);
+  const [user, setUser] = useState<any>(null);
+  const [currentWeather, setCurrentWeather] = useState<Weather>({
+    city: '',
+    tempC: '',
+    tempF: '',
+    weather: '',
+    icon: '',
+  });
   const [allAlarms, setAllAlarms] = useState<string[]>([
     "Classic analog alarm.mp3",
     "Classic digital alarm.mp3",
@@ -81,43 +102,9 @@ export const SettingsProvider: React.FC = ({ children }) => {
     "Tri-tone ping.mp3",
     "Xylophone.mp3",
   ]);
-  const [musicVolume, setMusicVolume] = useState<number>(100);
-  const [alertVolume, setAlertVolume] = useState<number>(100);
-  const [zip, setZip] = useState<string>('');
-  const [showSettings, setShowSettings] = useState<boolean>(false);
-  const [currentWeather, setCurrentWeather] = useState<Weather>({
-    city: '',
-    tempC: '',
-    tempF: '',
-    weather: '',
-    icon: '',
-  });
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-  const [started, setStarted] = useState<boolean>(false);
-  const [user, setUser] = useState<any>(null);
 
-  const getAlarms = () => {
-    axios.get("/api/getAlarms").then((data) => {
-      if (data.data.length) {
-        let alerts = data.data.filter((name) => name.includes(".mp3"));
-        setAllAlarms(alerts);
-        localStorage.setItem("allAlarms", JSON.stringify(alerts));
-      }
-    });
-  }
-
-  const checkMobile = () => {
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-    ) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-  }
   const store = {
+    isLoading: isLoading,
     isClock: isClock,
     setIsClock: (isClock: boolean): void => {
       setIsClock(isClock);
@@ -128,9 +115,9 @@ export const SettingsProvider: React.FC = ({ children }) => {
       setPomodoroTime(time);
       localStorage.setItem("pom", time.toString());
     },
-    shortBreakTime: shortBreakTime,
-    setShortBreakTime: (time: number): void => {
-      setShortBreakTime(time);
+    breakTime: breakTime,
+    setBreakTime: (time: number): void => {
+      setBreakTime(time);
       localStorage.setItem("short", time.toString());
     },
     autoStartBreak: autoStartBreak,
@@ -193,57 +180,53 @@ export const SettingsProvider: React.FC = ({ children }) => {
   };
 
   useEffect((): any => {
-    const cachedClock = localStorage.getItem("isClock");
-    const cachedPom = localStorage.getItem("pom");
-    const cachedShort = localStorage.getItem("short");
-    const cachedAuto = localStorage.getItem("auto");
-    const cachedSeconds = localStorage.getItem("showSeconds");
-    const cached24 = localStorage.getItem("24");
-    const cachedAlert = localStorage.getItem("alert");
-    const cachedAlarms = localStorage.getItem("allAlarms");
-    const cachedMusicVol = localStorage.getItem("musicVolume");
-    const cachedAlertVol = localStorage.getItem("alertVolume");
-    const cachedZip = localStorage.getItem("zip");
-    if (cachedClock) {
-      store.setIsClock(JSON.parse(cachedClock));
-    }
-    if (cachedPom) {
-      store.setPomodoroTime(parseInt(cachedPom));
-    }
-    if (cachedShort) {
-      store.setShortBreakTime(parseInt(cachedShort));
-    }
-    if (cachedAuto) {
-      store.setAutoStartBreak(JSON.parse(cachedAuto));
-    }
-    if (cachedSeconds) {
-      store.setShowSeconds(JSON.parse(cachedSeconds));
-    }
-    if (cached24) {
-      store.setIs24Hour(JSON.parse(cached24));
-    }
-    if (cachedAlert) {
-      store.setSelectedAlert(cachedAlert);
-    }
-    if (cachedAlarms) {
-      store.setAllAlarms(JSON.parse(cachedAlarms));
-    }
-    if (cachedMusicVol) {
-      store.setMusicVolume(parseInt(cachedMusicVol));
-    }
-    if (cachedAlertVol) {
-      store.setAlertVolume(parseInt(cachedAlertVol));
-    }
-    if (cachedZip) {
-      store.setZip(cachedZip);
-    }
-
-    getAlarms();
-    checkMobile();
     firebase.auth().onAuthStateChanged(user => {
       setUser(user);
     })
+    getAlarms(setAllAlarms);
+    checkMobile(setIsMobile);
+    setIsLoading(false);
   }, []);
+
+  useEffect(() => {
+    if (user) getSettingsFromFirebase(store, user);
+    else getSettingsFromLocalStorage(store);
+  }, [user])
+
+  useEffect(() => {
+    if (!showSettings && user && !isLoading) {
+      console.log("Post general settings to DB");
+      updateDB();
+    }
+  }, [showSettings])
+
+  const updateDB = async () => {
+    const data = {
+      pomodoroTime: JSON.stringify(pomodoroTime),
+      breakTime: JSON.stringify(breakTime),
+      autoStartBreak: JSON.stringify(autoStartBreak),
+      showSeconds: JSON.stringify(showSeconds),
+      is24Hour: JSON.stringify(is24Hour),
+      selectedAlert: selectedAlert,
+      alarms: JSON.stringify(allAlarms),
+      musicVolume: JSON.stringify(musicVolume),
+      alertVolume: JSON.stringify(alertVolume),
+      zip: zip
+    }
+
+    try {
+      const docRef = doc(db, user.uid, "settings");
+      setDoc(docRef, data).then(() => {
+        getSettingsFromFirebase(store, user);
+      })
+        .catch(error => {
+          console.log(error);
+        })
+
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
 
   return (
     <SettingsContext.Provider value={store}>
