bc840149b7d2392f284bb0c2427cfac64a055211
Seiji Matsumoto <seijim27@gmail.com>
2023-08-05 22:04:35 -0400

    Update greeting

--- src/Components/Settings/Login.tsx
+++ src/Components/Settings/Login.tsx
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { SettingsContext } from "../../contexts/SettingsContext";
 import { signInWithGoogle, signOutWithGoogle } from "../../firebase";
 import styles from '../../styles/Settings/Login/Login.module.css';
 
 const Login = () => {
+  const [greeting, setGreeting] = useState<string>("Hello");
   const {
     user
   } = useContext(SettingsContext);
@@ -13,6 +14,21 @@ const Login = () => {
     localStorage.clear();
     location.reload();
   }
+
+  useEffect(() => {
+    const d = new Date();
+    const time = d.getHours();
+    if (time > 6 && time < 12) {
+      setGreeting("Good morning, ");
+    } else if (time > 12 && time < 18) {
+      setGreeting("Good afternoon, ");
+    } else if (time >= 18 && time <= 21) {
+      setGreeting("Good evening, ");
+    } else {
+      setGreeting("Hello, ");
+    }
+  }, [])
+
 
   return (
     <div className={styles.login}>
@@ -25,7 +41,7 @@ const Login = () => {
         </div>
       ) : (
         <div className={styles.loggedInWrapper}>
-          <span className={styles.greeting}>Hello, {user['displayName'].split(" ")[0]}</span>
+          <span className={styles.greeting}>{greeting}{user['displayName'].split(" ")[0]}!</span>
           <button className={styles.logoutBtn} onClick={signout}>
             Sign out
           </button>
